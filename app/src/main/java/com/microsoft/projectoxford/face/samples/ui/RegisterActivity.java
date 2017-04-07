f4aeef6ab6f979a4dfcbe635fc95008f5405357e
DipeanraCFC <rawbean.dt@gmail.com>
2017-04-07 15:35:59 +0545

    Password reset successful.

--- app/src/main/java/com/microsoft/projectoxford/face/samples/ui/RegisterActivity.java
+++ app/src/main/java/com/microsoft/projectoxford/face/samples/ui/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -172,6 +173,8 @@ public class RegisterActivity extends AppCompatActivity {
         }
 
     }*/
+
+
 
 
     public void newEmailPassword() {
